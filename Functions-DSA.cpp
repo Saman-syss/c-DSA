2c38b8c845fe78f7936102a143b36c4e3601c700
Saman-syss <146308264+Saman-syss@users.noreply.github.com>
2024-09-06 21:59:35 +0545

    Functions lesson code
    Uploaded Functions file

---
+++ Functions-DSA.cpp
@@ -0,0 +1,32 @@
+#include <iostream>
+using namespace std;
+
+//Function Defination
+// void printHello(){
+//     cout<<"Hello World!";
+// }
+
+// int main(){
+//     cin>>
+//     printHello(); //Function calling  / Invoke
+
+// }
+// int sum(int a,int b){
+//     int s = a + b ;
+//     return s;
+// }
+
+// int minOfTwo(int a , int b){
+//     if (a < b){
+//         return a;
+//     }else{
+//         return b;
+//     }
+// }
+int main(){
+    // cout<<"min = "<< minOfTwo(219,438);
+    // cout<<sum(10,15);
+
+
+    return 0;
+}
